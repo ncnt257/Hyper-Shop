238445dd186e3d3d11bf8930bf5d7a9caf3364fd
Tung Nguyen <ncnt257@gmail.com>
2022-03-01 20:20:36 +0700

    add library projects

---
+++ HyperShop.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HyperShop.Models;
+
+namespace HyperShop.DataAccess
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HyperShop.Models.Category> Category { get; set; }
+    }
+}
