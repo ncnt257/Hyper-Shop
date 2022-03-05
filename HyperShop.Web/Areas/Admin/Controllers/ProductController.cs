6f659aacdd8b2bc17519d9c7022362afc10ae7e1
Tung Nguyen <ncnt257@gmail.com>
2022-03-05 12:17:40 +0700

    resize upload image

--- HyperShop.Web/Areas/Admin/Controllers/ProductController.cs
+++ HyperShop.Web/Areas/Admin/Controllers/ProductController.cs
@@ -5,6 +5,9 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -16,6 +19,15 @@ namespace HyperShop.Web.Areas.Admin.Controllers
     [Area("Admin")]
     public class ProductController : Controller
     {
+        private void Image_resize(string input_Image_Path, string output_Image_Path, int width = 200, int height = 200)
+        {
+            using (var image = Image.Load(input_Image_Path))
+            {
+                image.Mutate(x => x
+                     .Resize(image.Width > width ? width : image.Width, image.Height > height ? height : image.Height));
+                image.Save(output_Image_Path);
+            }
+        }
         private  IWebHostEnvironment _hostEnvironment;
         private ApplicationDbContext _context;
         public ProductController(ApplicationDbContext context, IWebHostEnvironment hostEnvironment)
@@ -60,11 +72,13 @@ namespace HyperShop.Web.Areas.Admin.Controllers
                 string fileName = Guid.NewGuid().ToString();
                 var uploads = Path.Combine(wwwRootPath, @"img\products");
                 var extension = Path.GetExtension(file.FileName);
-
-                using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                string filePath = Path.Combine(uploads, fileName + extension);
+                using (var fileStreams = new FileStream(filePath, FileMode.Create))
                 {
                     file.CopyTo(fileStreams);
                 }
+                Image_resize(filePath, filePath);
+
                 product.PrimaryImage = @"\img\products\" + fileName + extension;
 
                 _context.Products.Add(product);
@@ -114,10 +128,10 @@ namespace HyperShop.Web.Areas.Admin.Controllers
                 {
                     string wwwRootPath = _hostEnvironment.WebRootPath;
                     string fileName = Guid.NewGuid().ToString();
-                    var uploads = Path.Combine(wwwRootPath, @"img\products");
-                    var extension = Path.GetExtension(file.FileName);
-
-                    using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                    string uploads = Path.Combine(wwwRootPath, @"img\products");
+                    string extension = Path.GetExtension(file.FileName);
+                    string filePath = Path.Combine(uploads, fileName + extension);
+                    using (var fileStreams = new FileStream(filePath, FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
                     }
@@ -132,6 +146,8 @@ namespace HyperShop.Web.Areas.Admin.Controllers
                         }
 
                     }
+
+                    Image_resize(filePath, filePath);
 
                     product.PrimaryImage = @"\img\products\" + fileName + extension;
 
