b89d48782284ed6bb849c38837423fd95b9e3c34
Tung Nguyen <ncnt257@gmail.com>
2022-03-11 18:23:37 +0700

    add filter card to products page for customer

---
+++ HyperShop.Models/ViewModels/ProductsPageVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HyperShop.Models.ViewModels
+{
+    public class ProductsPageVM
+    {
+        public List<Brand> Brands { get; set; }
+        public List<Category> Categories { get; set; }
+        public List<Color> Colors { get; set; }
+        public List<string> Genders { get; set; }
+        public List<string> ShoesHeightTypes { get; set; }
+        public List<string> ClosureTypes { get; set; }
+    }
+}
--- HyperShop.Web/Areas/Customer/Controllers/ProductController.cs
+++ HyperShop.Web/Areas/Customer/Controllers/ProductController.cs
@@ -1,5 +1,8 @@
 ﻿using HyperShop.DataAccess;
+using HyperShop.Models;
+using HyperShop.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Linq;
 
 namespace HyperShop.Web.Areas.Customer.Controllers
@@ -15,8 +18,67 @@ namespace HyperShop.Web.Areas.Customer.Controllers
         }
         public IActionResult Index()
         {
-            var productList = _context.Products.ToList();
-            return View(productList);
+
+
+
+            ProductsPageVM productsPageVM = new ProductsPageVM()
+            {
+                Brands = _context.Products
+                .Include(p => p.Brand)
+                .ToList()
+                .GroupBy(p => p.Brand)
+                
+                .OrderByDescending(grp => grp.Count())
+                .Take(5)
+                
+                .Select(p => new Brand()
+                {
+                    Id = p.Key.Id,
+                    Name = p.Key.Name
+                }).ToList(),
+                Categories = _context.Products
+                .Include(p => p.Category)
+                .ToList()
+                .GroupBy(p => p.Category)
+                .OrderByDescending(grp => grp.Count())
+                .Take(5)
+                .Select(p => new Category()
+                {
+                    Id = p.Key.Id,
+                    Name = p.Key.Name
+                }).ToList(),
+
+                Colors = _context.Stock
+                .Include(p => p.Product)
+                .Include(p => p.Color)
+                .ToList()
+                .GroupBy(s => new { s.Product, s.Color })
+                .GroupBy(s => s.Key.Color)
+                .OrderByDescending(grp => grp.Count())
+                .Take(5)
+                .Select(p => new Color { Id = p.Key.Id, ColorValue = p.Key.ColorValue})
+                .ToList(),
+
+                ShoesHeightTypes = _context.Products
+                .ToList()
+                .GroupBy(p => p.ShoesHeight)
+                .OrderByDescending(grp => grp.Count())
+                .Take(5)
+                .Select(p => p.Key).ToList(),
+
+                ClosureTypes = _context.Products.ToList()
+                .GroupBy(p => p.ClosureType)
+                .OrderByDescending(grp => grp.Count())
+                .Take(5)
+                .Select(p => p.Key).ToList(),
+
+                Genders = _context.Products.ToList()
+                .GroupBy(p => p.Gender)
+                .OrderByDescending(grp => grp.Count())
+                .Take(5)
+                .Select(p => p.Key).ToList(),
+            };
+            return View(productsPageVM);
         }
     }
 }
