e4c53c7fba162264a3307a0676f4645d2f1d4287
Tung Nguyen <ncnt257@gmail.com>
2022-03-16 08:54:48 +0700

    product filters

--- HyperShop.Web/Areas/Customer/Controllers/ApiController.cs
+++ HyperShop.Web/Areas/Customer/Controllers/ApiController.cs
@@ -1,4 +1,5 @@
 ﻿using HyperShop.DataAccess;
+using HyperShop.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,10 +17,42 @@ namespace HyperShop.Web.Areas.Customer.Controllers
             _context = context;
         }
 
-        public IActionResult Products(int page, List<int> categories, List<int> bands, List<int> colors, List<string> shoesHeights, List<string> closureTypes, List<string> genders)
+        public IActionResult Products(int page,int taking, string orderBy, List<int> categories, List<int> brands, List<int> colors, List<string> shoesHeights, List<string> closureTypes, List<string> genders)
         {
-            var products = _context.Products.Skip(3 * page).Take(3).ToList();
-            return Json(products);
+            if (page == 0) page = 1;
+            if (taking == 0) taking = 12;
+            if (orderBy == null) orderBy = "PublishedDate";
+
+            IEnumerable<Product> products = _context.Products;
+
+            if (categories != null && categories.Count>0) products = products.Where(p => categories.Contains(p.CategoryId.Value));
+            if (brands != null && brands.Count > 0) products = products.Where(p => brands.Contains(p.BrandId.Value));
+            if (shoesHeights != null && shoesHeights.Count > 0) products = products.Where(p => shoesHeights.Contains(p.ShoesHeight));
+            if (closureTypes != null && closureTypes.Count > 0) products = products.Where(p => closureTypes.Contains(p.ClosureType));
+            if (genders != null && genders.Count > 0) products = products.Where(p => genders.Contains(p.Gender));
+            if (colors != null && colors.Count > 0) products = products.Where(p => _context.Stock.Any(s => s.ProductId == p.Id && colors.Contains(s.ColorId)));
+
+            int productsCount = products.Count();
+
+            products = products.OrderBy(p => p.GetType().GetProperty(orderBy).GetValue(p))
+                .Skip(taking * (page - 1))
+                .Take(taking);
+
+            var productList = products
+                .Select(p=> new { 
+                    Id=p.Id,
+                    Name=p.Name,
+                    Price = p.Price,
+                    PrimaryImage = p.PrimaryImage,
+                    ColorCount= _context.Stock.ToList()
+                        .GroupBy(s => new { s.ProductId, s.ColorId })
+                        .Where(s => s.Key.ProductId == p.Id)
+                        .Count()
+                })
+                .ToList();
+
+
+            return Json(new { productList, productsCount });
         }
     }
 }
--- HyperShop.Web/wwwroot/js/loadProduct.js
+++ HyperShop.Web/wwwroot/js/loadProduct.js
@@ -1,24 +1,138 @@
+let page = 1;
+let taking = 3;
+let categories = [];
+let brands = [];
+let colors = [];
+let shoesHeights = [];
+let closureTypes = [];
+let genders = [];
+
+let orderBy = "Price";
+
+
 $(document).ready(function () {
-    loadProducts(1)
+    loadProducts()
 });
 
-function loadProducts(page, categories, brands, colors, shoesHeights, closureType, genders){
+$('.filter').on('click', function(e){
+    e.preventDefault();
+    e.stopPropagation();
+    if (this.classList.contains("category-filter")) {
+        categories = [];
+        $('.category-form input:checked').each(function () {
+            categories.push($(this).val());
+        })
+    }
+    else if (this.classList.contains("brand-filter")) {
+        brands = [];
+        $('.brand-form input:checked').each(function () {
+            brands.push($(this).val());
+        })
+    }
+    else if (this.classList.contains("color-filter")) {
+        colors = [];
+        $('.color-form input:checked').each(function () {
+            colors.push($(this).val());
+        })
+    }
+    else if (this.classList.contains("shoes-height-filter")) {
+        shoesHeights = [];
+        $('.shoes-height-form input:checked').each(function () {
+            shoesHeights.push($(this).val());
+        })
+    }
+    else if (this.classList.contains("closure-type-filter")) {
+        closureTypes = [];
+        $('.closure-type-form input:checked').each(function () {
+            closureTypes.push($(this).val());
+        })
+    }
+    else if (this.classList.contains("gender-filter")) {
+        genders = [];
+        $('.gender-form input:checked').each(function () {
+            genders.push($(this).val());
+        })
+    }
+
+    loadProducts();
+});
+
+
+$('.clear').on('click', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if (this.classList.contains("category-clear")) {
+        categories = [];
+        $('.category-form input:checked').each(function () {
+            $(this).prop("checked", false);
+        })
+    }
+    else if (this.classList.contains("brand-clear")) {
+        brands = [];
+        $('.brand-form input:checked').each(function () {
+            $(this).prop("checked", false);
+        })
+    }
+    else if (this.classList.contains("color-clear")) {
+        colors = [];
+        $('.color-form input:checked').each(function () {
+            $(this).prop("checked", false);
+        })
+    }
+    else if (this.classList.contains("shoes-height-clear")) {
+        shoesHeights = [];
+        $('.shoes-height-form input:checked').each(function () {
+            $(this).prop("checked", false);
+        })
+
+    }
+    else if (this.classList.contains("closure-type-clear")) {
+        closureTypes = [];
+        $('.closure-type-form input:checked').each(function () {
+            $(this).prop("checked", false);
+        })
+    }
+    else if (this.classList.contains("gender-clear")) {
+        genders = [];
+        $('.gender-form input:checked').each(function () {
+            $(this).prop("checked", false);
+        })
+    }
+
+    loadProducts();
+});
+
+
+
+
+
+
+
+//utility
+function loadProducts(){
     $.ajax({
         url: "/customer/api/products",
         traditional: true,
         data: {
             page,
+            taking,
+            orderBy,
             categories,
             brands,
             colors,
             shoesHeights,
-            closureType,
+            closureTypes,
             genders, 
 
         },
         dataType: "json",
         success: function (res) {
-            $('.products').html(getProductList(res));
+            lastPage = Math.floor(res.productsCount / taking);
+
+            $('.products').html(getProductList(res.productList));
+            $('.pages').html(getPagesNumber(lastPage, page));
+            $('.products-showing').html(getProductsShowing(res.productList.length, res.productsCount));
+
         },
         error: function (err) {
             console.log("failed")
@@ -28,14 +142,14 @@ function loadProducts(page, categories, brands, colors, shoesHeights, closureTyp
 }
 function getProductList(products) {
     let res = '';
-    for (product of products) {
+    for (product of products) { 
         res += getProduct(product)
     }
     return res;
 }
 
 function getProduct(product) {
-    return `
+    let res =  `
         <div class="col-lg-4 col-md-6">
                   <div class="product">
                     <div class="flip-container">
@@ -46,34 +160,73 @@ function getProduct(product) {
                         </div>
                       </div>
                     </div><a href="detail.html" class="invisible"><img src="${product.primaryImage}" alt="" class="img-fluid"></a>
-                     <table>
-                               <tr>
-                                   <td><a href="#1"><img src="${product.primaryImage}" alt="" class="img-fluid" style="width:50px;"></a> </td>
-                                   <td><a href="#2"><img src="${product.primaryImage}" alt="" class="img-fluid" style="width:50px;"></a> </td>
-                                   <td><a href="#3"><img src="${product.primaryImage}" alt="" class="img-fluid" style="width:50px;"></a> </td>
-
-
-                               </tr>
-                           </table>
+                     
                     <div class="text">
                       <h3><a href="detail.html">${product.name}</a></h3>
+                      <p class = "p-3 m-1 bg-primary text-white">
+                        ${product.colorCount} COLOR
+                      </p>
                       <p class="price">
                         ${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}
                       </p>
                       <p class="buttons"><a href="detail.html" class="btn btn-outline-secondary">View detail</a><a href="basket.html" class="btn btn-primary"><i class="fa fa-shopping-cart"></i>Add to cart</a></p>
                     </div>
                     <!-- /.text-->
- </div>
-                  <!-- /.product            -->
-                </div>
+
 `
 
-                    //if((DateTime.Now-item.PublishedDate).TotalDays < 7){
-                    //        <div class="ribbon new">
-                    //            <div class="theribbon">NEW</div>
-                    //            <div class="ribbon-background"></div>
-                    //        </div>
-                    //    }
+    if (datediff(Date.now(), Date.parse(product.publishedDate)) < 7) {
+        res += `
+        <div class="ribbon sale">
+            <div class="theribbon">NEW</div>
+            <div class="ribbon-background"></div>
+        </div>`
+    }
+    res += `
+        </div>
+            <!-- /.product -->
+        </div>`
+    return res;
+}
 
-                 
+function getPagesNumber(lastPage, page) {
+    let res = `
+<nav
+  aria-label="Page navigation example"
+  class="d-flex justify-content-center"
+>`;
+    if (lastPage > 0) {
+        res += `<ul class="pagination"><li class="page-item"><a href="#" class="page-link">First</a></li>`;
+        let i = Number(page) > 5 ? Number(page) - 4 : 1;
+        if (i !== 1) {
+            res += `<li class="page-item"><a href="#" class="page-link">...</a></li>`;
+        }
+        for (; i <= Number(page) + 4 && i <= lastPage; i++) {
+            if (i == page) {
+                res += `<li class="page-item active"><a href="#" class="page-link">${i}</a></li>`;
+            } else {
+                res += `<li class="page-item"><a href="#" class="page-link">${i}</a></li>`;
+            }
+            if (i == Number(page) + 4 && i < lastPage) {
+                res += `<li class="page-item"><a href="#" class="page-link">...</a></li>`;
+            }
+        }
+        res += `<li class="page-item">
+  <a href="#" class="page-link">Last</a>
+</li>`;
+        res += `</ul>`;
+    }
+
+    res += `</nav>`;
+    return res;
+}
+
+function getProductsShowing(count, total) {
+    return `
+    Showing <strong>${count}</strong> of <strong>${total}</strong> products
+`
+}
+
+function datediff(first, second) {
+    return Math.round((second - first) / (1000 * 60 * 60 * 24));
 }
