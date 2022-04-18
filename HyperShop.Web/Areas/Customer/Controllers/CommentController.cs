5635737cf0511b6e6ca456f3ef2283d1ef18d7ca
Tung Nguyen <ncnt257@gmail.com>
2022-04-18 21:48:04 +0700

    post comment

---
+++ HyperShop.Web/Areas/Customer/Controllers/CommentController.cs
@@ -0,0 +1,40 @@
+﻿using HyperShop.DataAccess;
+using HyperShop.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Security.Claims;
+
+namespace HyperShop.Web.Areas.Customer.Controllers
+{
+    [Authorize]
+    [Area("Customer")]
+    public class CommentController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CommentController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        #region API CALLS
+
+        [HttpPost]
+        public IActionResult Post(int productId, string body)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var comment = new Comment()
+            {
+                ProductId = productId,
+                Body = body,
+                ApplicationUserId = userId
+            };
+            _context.Comments.Add(comment);
+            comment.ApplicationUser = _context.ApplicationUsers.First(u=>u.Id==userId);
+            return Json(comment);
+        }
+        #endregion
+
+    }
+}
---
+++ HyperShop.Web/wwwroot/js/comment.js
@@ -0,0 +1,44 @@
+﻿$('.comment-form').on('submit', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+
+    const body = $('.comment-input').val();
+    const productId = $(this).attr('id');
+    const url = $(this).attr('action');
+
+    $.ajax({
+        url,
+        method: "post",
+        data: {
+            body,
+            productId
+        },
+        success: function (data) {
+            $('.comments-list').append(renderComment(data));
+        }
+    })
+
+
+})
+
+function renderComment(comment) {
+    return `
+     <div class="comment">
+      <div class="customer-avatar">
+        <i class="far fa-user-circle medium-text"></i>&nbsp; &nbsp;<span>${comment.applicationUser.fullName}</span>
+      </div>
+      <div class="comment-content">
+        <span class="light-gray-color">${comment.body}</span>
+      </div>
+
+
+
+
+    <form class="response-form" action="/api/response/" method="post" autocomplete="off">
+      <input type="text" class="rep-comment" name="response" placeholder="Reply comment .....">
+      <button hidden></button>
+    </form>
+      <div class="divide"></div>
+    </div>
+    `
+}
